17c41a882110a2b203794b82b235fbf82dead939
Luca Ruberto s247125 <s247125@studenti.polito.it>
2022-06-09 11:55:09 +0200

    fix login

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Router, Routes, Route, Navigate, useNavigate, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import './App.css';
 import { LoginForm, LogoutButton } from './components/LoginComponents';
 import { Container } from 'react-bootstrap/';
-import { useEffect, useState, navigate } from 'react';
+import { useEffect, useState } from 'react';
 import API from './API';
 import { CoursesList } from './components/CoursesList';
 
-
 function App() {
+  return (
+    <Router>
+      <App2 />
+    </Router>
+  )
+}
+function App2() {
   const [courses, setCourses] = useState([]); //Lista di corsi versione Client-Server
   const [loggedIn, setLoggedIn] = useState(false);  // no user is logged in when app loads
   const [user, setUser] = useState({});
   const [message, setMessage] = useState('');
   const [dirty, setDirty] = useState(true);
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     const checkAuth = async () => {
       try {
@@ -30,15 +39,14 @@ function App() {
     checkAuth();
   }, []);
 
-  useEffect(() => {
+  /*useEffect(() => {
     if (loggedIn)
       API.getAllCourses()
         .then((courses) => { setCourses(courses); setDirty(true); })
         .catch(err => handleError(err))
-  }, [loggedIn])
+  }, [loggedIn])*/
 
   useEffect(() => {
-
     API.getAllCourses().then((list) => { setCourses(list) })
       .catch(err => handleError(err))
   }, [])
@@ -67,15 +75,11 @@ function App() {
   }
   return (
     <>
-      <BrowserRouter>
-        <Container fluid className="App">
-          <Routes>
-            <Route path='/' element={<CoursesList courses={courses} />} />
-            <Route path='/login' element={loggedIn ? <Navigate to='/' /> : <LoginForm login={doLogIn} />} />
-            <Route path='/home-logged ' />
-          </Routes>
-        </Container>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<CoursesList courses={courses} />} />
+        <Route path='/login' element={loggedIn ? <Navigate to='/' /> : <LoginForm login={doLogIn} />} />
+        <Route path='/home-logged ' />
+      </Routes>
 
     </>
   );
